d9161bcde53917b3c18b3ad7bc511e959d6cde87
yymmhh <yymmhh@126.com>
2020-09-07 17:30:31 +0800

    初始化

---
+++ src/index.js
@@ -0,0 +1,143 @@
+const md =  '# 我是一级标题\n' + 
+            '## 我是二级标题\n' +
+            '### 我是三级标题\n' +
+            '[**Showdown**](http://www.showdownjs.com) is *great*\n' +
+            '\nbecause:\n' +
+            ' - it\'s easy to use\n' +
+            ' - it\'s extensible\n' +
+            ' - works in the server and in the browser';
+
+            const html = `
+            <div style="margin: 10px 0 10px;">
+              <b>* 字体标签</b>
+            </div>
+            <h1>h1</h1>
+            <h2>h2</h2>
+            <h3>h3</h3>
+            <h4>h4</h4>
+          
+            <div style="margin: 10px 0 10px;">
+              <b>* video标签</b>
+            </div>
+            <video src="http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"></video>
+          
+            <div style="margin: 10px 0 10px;">
+              <b>* audio标签</b>
+            </div>
+            <audio title="我是标题" desc="我是小标题" src="https://media.lycheer.net/lecture/25840237/5026279_1509614610000.mp3?0.1"></audio>
+          
+            <div style="margin: 10px 0 10px;">
+            <b>一. 转义字符</b>
+            </div>&lt;div style=&quot;color:red&quot;&gt;我是转义字符&lt;/div&gt; 
+          
+            <div style="margin: 10px 0 10px;">
+            <b>二. a标签跳转</b>
+            </div> 
+            <div>
+              <a href="https://www.baidu.com" title="我是外链跳转" target="_blank">a标签跳转（外链跳转）</a>&nbsp; 
+            </div>
+            <div>
+              <a href="/pages/index/index" title="我是内链跳转" target="_blank">a标签跳转（内链跳转）</a>&nbsp; 
+            </div>
+          
+            <div style="margin: 10px 0 10px;">
+            <b>三. 内联标签</b>
+            </div> 
+            <span style="background-color: rgb(139, 170, 74);">我是内联标签</span>
+            <br />
+            <p></p>
+            <p></p>
+          
+            <div style="margin: 10px 0 10px;">
+            <b>四. ul无序列表</b>
+            </div>
+            <ul>
+            <li style="text-align: center;"><span style="background-color: rgb(139, 170, 74);">1</span></li>
+            </ul>
+          
+            <div style="margin: 10px 0 10px;">
+            <b>五. ol有序列表</b>
+            </div>
+            <ol>
+            <li style="text-align: center;"><span style="background-color: rgb(139, 170, 74);">test</span></li>
+            <li><span style="background-color: rgb(139, 170, 74);">test2</span></li>
+            </ol>
+          
+            <div style="margin: 10px 0 10px;">
+            <b>六. 表格渲染</b>
+            </div>
+            <table>
+              <tr>
+                <th>标题1</th>
+                <th>标题2</th>
+                <th>标题3</th>
+                <th>标题4</th>
+                <th>标题5</th>
+              </tr>
+              <tr>
+                <td>cell1</td>
+                <td>cell2</td>
+                <td>cell3</td>
+                <td>cell4</td>
+                <td>cell5</td>
+              </tr>
+              <tr>
+                <td>cell1</td>
+                <td>cell2</td>
+                <td>cell3</td>
+                <td>cell4</td>
+                <td>cell5</td>
+              </tr>
+            </table>
+          
+            <div style="margin: 10px 0 10px;">
+            <b>七. 图片渲染</b>
+            </div>
+            <p><img src="https://dev-sit-1251698455.cos.ap-guangzhou.myqcloud.com/ds/22/363/20200401/3c9e7798e3204756b9e0f3263882b81f.jpeg" /><img src="https://mmbiz.qpic.cn/mmbiz_png/1gmcynicwloGkVMTr6wTHdDXlFUSaSxOSRELianAFGJYVzvXJKoM2xbbFMqKe6ONy5zoHHejNbibTJn5gdEOc1aIA/0?wx_fmt=png" width="200" height="100" style="margin: 0 auto;" /></p>
+          `
+
+const jsCode = 
+`init = jQuery.fn.init = function( selector, context, root ) {
+  var match, elem;
+
+  // v1.4之后，可以创建不包含任何DOM节点的空jQuery对象
+  if ( !selector ) {
+    return this
+  }
+
+  // 处理字符串
+  if ( typeof selector === 'string' ) {
+    // 处理 HTML 字符串
+    if ( selector[ 0 ] === '<' && selector[ selector.length - 1 ] === '>' && selector.length >= 3) {
+        // ....
+
+    // 其它字符串，当做选择器处理    
+    } else {
+      match = rquickExpr.exec( selector )
+    }
+
+  // 处理 DOM 节点 => $(DOMElement)  
+  } else if ( selector.nodeType ) {
+    // .....
+
+  // 处理函数 => $(function)
+  } else if ( typeof selector === 'function' ) {
+    // ....
+  }
+
+  // 处理其它情况
+  return jQuery.makeArray( selector, this )
+}`
+
+Component({
+  data: {
+    codeText: jsCode,
+    htmlText: html,
+    mdText: md,
+  },
+
+  methods: {
+    handleTagATap() {
+    }
+  }
+})
---
+++ src/wxParse.js
@@ -0,0 +1,158 @@
+import HtmlToJson from './utils/html2json';
+import showdown from './utils/showdown.js';
+import { getSystemInfo, bindInstance } from './utils/util';
+
+Component({
+  properties: {
+    nodes: {
+      type: null,
+      observer(val) {
+        const { language } = this.properties
+        if (val) {
+          if (language === 'html') {
+            this._parseNodes(val)
+          }
+
+          if (language === 'markdown' || language === 'md') {
+            const converter = new showdown.Converter();
+            const parseNodes = converter.makeHtml(val);
+            this._parseNodes(parseNodes)
+          }
+        }
+      }
+    },
+
+    language: {
+      type: String,
+      value: 'html' // 可选：html | markdown (md)
+    }
+  },
+
+  data: {
+    nodesData: [],
+    bindData: {},
+  },
+
+  detached() {
+    // 组件销毁，清除绑定实例
+    bindInstance.clear()
+  },
+
+  methods: {
+    _parseHtml(html, bindName) {
+      bindName = 'wxParseData'
+
+      //存放html节点转化后的json数据
+      const transData = HtmlToJson.html2json(html, bindName)
+
+      transData.view = {}
+      transData.view.imagePadding = 0
+      this.setData({
+        nodesData: transData.nodes,
+        bindData: {
+          [bindName]: transData
+        }
+      })
+      bindInstance.set(bindName, transData)
+      console.log(this.data)
+    },
+
+    _parseNodes(nodes) {
+      if (typeof nodes === 'string') { // 初始为html富文本字符串
+        this._parseHtml(nodes)
+      } else if (Array.isArray(nodes)) { // html 富文本解析成节点数组
+        this.setData({ nodesData: nodes })
+      } else { // 其余为单个节点对象
+        const nodesData = [ nodes ]
+        this.setData({ nodesData })
+      }
+    },
+
+    /**
+     * 图片视觉宽高计算函数区
+     * @param {*} e 
+     */
+    wxParseImgLoad(e) {
+      // 获取当前的image node节点
+      const { from: tagFrom, index } = e.target.dataset || {}
+      if (typeof tagFrom !== 'undefined' && tagFrom.length > 0) {
+        const { width, height } = e.detail
+        
+        //因为无法获取view宽度 需要自定义padding进行计算，稍后处理
+        const recal = this._wxAutoImageCal(width, height)
+        this.setData({
+          width: recal.imageWidth,
+          height: recal.imageHeight,
+          [`nodesData[${index}].loaded`]: true,
+        })
+      }
+    },
+
+    /**
+     * 预览图片
+     * @param {*} e 
+     */
+    wxParseImgTap(e) {
+      const { src } = e.target.dataset
+      const bindName = 'wxParseData'
+      const { imageUrls } = bindInstance.set(bindName)
+      wx.previewImage({ 
+        current: src,
+        urls: imageUrls
+      })
+    },
+
+    /**
+     * 计算视觉优先的图片宽高
+     * @param {*} originalWidth 
+     * @param {*} originalHeight 
+     */
+    _wxAutoImageCal(originalWidth, originalHeight) {
+      let autoWidth = 0, autoHeight = 0;
+      const results = {}
+      const [ windowWidth, windowHeight ] = getSystemInfo()
+
+      // 判断按照哪种方式进行缩放
+      if (originalWidth > windowWidth) { //在图片width大于手机屏幕width时候
+        autoWidth = windowWidth
+        autoHeight = (autoWidth * originalHeight) / originalWidth
+        results.imageWidth = autoWidth
+        results.imageHeight = autoHeight
+      } else { // 否则展示原来数据
+        results.imageWidth = originalWidth
+        results.imageHeight = originalHeight
+      }
+      return results
+    },
+
+    /**
+     * 增加a标签跳转
+     * 1. 如果page页面有handleTagATap事件，优先采用事件回调的方式处理
+     * 2. 如果page页面没有handleTagATap事件，根据链接字段判断采用内外链跳转方式
+     * @param {*} e 
+     */
+    wxParseTagATap(e) {
+      const { src = '' } = e.currentTarget.dataset
+
+      // 采用递归组件方式渲染，不能通过triggerEvent方式向父级传参，可以获取当前页面调用页面方法处理
+      const curPages =  getCurrentPages();
+      const currentPage = curPages[curPages.length - 1]
+      if (currentPage && currentPage.handleTagATap) {
+        currentPage.handleTagATap(src)
+        return
+      }
+
+      // 判断是否内部链接跳转
+      const isInnerPage = src.indexOf('http') === -1
+      if (isInnerPage) {
+        wx.navigateTo({
+          url: src
+        })
+      } else {
+        wx.navigateTo({
+          url: `/miniprogram_npm/wx-minicomponent/wxParse/webviewPage/webviewPage?src=${src}`
+        })
+      }
+    }
+  }
+})
